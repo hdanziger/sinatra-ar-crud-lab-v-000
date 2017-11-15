5af55f840ebeec5a9aa587338436995b42599c2c
Hannah Danziger <hdanziger8@gmail.com>
2017-11-15 18:16:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,8 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/posts/:id/delete' do
+  post '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
     erb :delete
   end
 
